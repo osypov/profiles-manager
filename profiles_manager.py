fd25b1fbbfef26ce3ec980e069c854a8d3285e6a
Yaroslav1233 <osyaroslav098@gmail.com>
2024-01-25 11:44:42 +0000

    commit in profiles_manager

--- profiles_manager.py
+++ profiles_manager.py
@@ -3,16 +3,18 @@ from pathlib import Path
 import shutil
 import info
 from driver.main import d
-from .cli import Client
+# from .cli import Client
 
 class Manager:
-    def __init__(self):
+    def __init__(self, proxy=None):
         self.sorted_profiles = []
         self.profiles_dir = Path("profiles").absolute()
         self.profile_list = self.profiles_dir.joinpath("profile_list.txt")
 
 
         self.current_list = self.get_profiles_list()
+
+        self.proxy = proxy
 
     def get_profile_path_by_name(self, name):
         return self.profiles_dir.joinpath(name).absolute()
@@ -31,9 +33,11 @@ class Manager:
         path = self.get_profile_path_by_name(profile)
         self.path = path
         self.write_profile_if_not_exists(profile)
-        if proxy:
-            proxy = info.Proxies[0]
-            self.proxy = proxy
+        # if proxy:
+        #     proxy = info.Proxies[0]
+        #     self.proxy = proxy
+
+        proxy = self.proxy
 
         
         print(path)
@@ -72,10 +76,6 @@ class Manager:
     def clear_all(self, ):
         shutil.rmtree(self.profiles_dir)
         os.mkdir(self.profiles_dir)
-
-    def cli(self, script, args):
-        Client(self, script, args)
-
 
 
     
